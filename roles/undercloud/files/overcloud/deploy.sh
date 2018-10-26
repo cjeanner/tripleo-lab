70fea6369d649a9510790364df7355dafe9e6958
Cédric Jeanneret <cjeanner@redhat.com>
2018-10-26 18:15:27 +0200

    create deploy log by default

--- roles/undercloud/files/overcloud/deploy.sh
+++ roles/undercloud/files/overcloud/deploy.sh
@@ -11,7 +11,7 @@ openstack overcloud deploy \
   -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
-  --ntp-server pool.ntp.org --libvirt-type qemu
+  --ntp-server pool.ntp.org --libvirt-type qemu 2>&1 | tail -a ~/deploy-overcloud.log
 ret_val=$?
 
 if [ $ret_val -ne 0 ] && [ -n $1 ] && [ "$1" = '--delete-if-fail' ]; then
