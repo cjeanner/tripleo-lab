1eb7ec8d5e9c7fa812bf35fc9b461c3c7f9f527d
Cédric Jeanneret <cjeanner@redhat.com>
2019-02-06 15:02:23 +0100

    New script allowing to cleanup the whole undercloud/standalone env.

---
+++ roles/undercloud/files/env-cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo docker rm -f $(docker ps -aq )
+sudo podman rm -fa
+sudo rm -rf /var/lib/config-data/ \
+  /var/lib/docker-puppet/ \
+  /var/lib/rabbitmq \
+  /var/lib/mysql/ \
+  /var/lib/pacemaker/*
