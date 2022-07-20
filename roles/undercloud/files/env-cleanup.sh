b0219f3559fa637c14f0d05bffbaed528d25f8cf
Cédric Jeanneret <cjeanner@redhat.com>
2022-07-20 08:53:10 +0200

    Ensure nftables is cleaned

--- roles/undercloud/files/env-cleanup.sh
+++ roles/undercloud/files/env-cleanup.sh
@@ -7,5 +7,7 @@ sudo rm -rf /var/lib/config-data/ \
   /var/lib/mysql/ \
   /var/lib/pacemaker/* \
   /var/lib/image-serve/* \
+  /etc/nftables/tripleo-*.nft \
+  /etc/nftables/iptables.nft \
   /home/stack/*
 sudo nft flush ruleset
