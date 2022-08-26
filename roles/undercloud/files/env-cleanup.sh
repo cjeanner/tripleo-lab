95767de7628fa20d00a231d933ce0df0eb6bfbbf
Cédric Jeanneret <cjeanner@redhat.com>
2022-08-26 14:09:57 +0200

    Ensure we clean everything

--- roles/undercloud/files/env-cleanup.sh
+++ roles/undercloud/files/env-cleanup.sh
@@ -7,7 +7,13 @@ sudo rm -rf /var/lib/config-data/ \
   /var/lib/mysql/ \
   /var/lib/pacemaker/* \
   /var/lib/image-serve/* \
-  /etc/nftables/tripleo-*.nft \
+  /etc/nftables/tripleo-chains.nft \
+  /etc/nftables/tripleo-flushes.nft \
+  /etc/nftables/tripleo-jumps.nft \
+  /etc/nftables/tripleo-rules.nft \
+  /etc/nftables/tripleo-update-jumps.nft \
   /etc/nftables/iptables.nft \
   /home/stack/*
 sudo nft flush ruleset
+sudo sed -i '/^include /d' /etc/sysconfig/nftables.conf
+sudo systemctl disable --now nftables
