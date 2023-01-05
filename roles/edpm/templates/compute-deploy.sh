fafc95dd16471794c685457ce90603832cf04f9d
Cédric Jeanneret <cjeanner@redhat.com>
2023-01-05 13:56:19 +0100

    Some more work on EDPM support
    Now we get almost there - there's still some tweaking needed, but
    mostly, things are getting in place.
    
    Current issue: remote node loses its "public" connection when we run the
    os-net-config step of the actual deploy - more than probably an issue
    with the passed parameters.
    
    Some changes are also needed in tripleo-ansible, but they are being
    worked around in the current edpm role here.

---
+++ roles/edpm/templates/compute-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+test -f /home/stack/prepare-edpm.log && mv /home/stack/prepare-edpm.log /home/stack/prepare-edpm.log.$(date +%F-%R)
+ansible-playbook -i /home/stack/tripleo-deploy/edpm-inventory/ /home/stack/edpm-prepare-playbook.yaml >&1 | tee /home/stack/prepare-edpm.log
+
+test -f /home/stack/deploy-edpm.log && mv /home/stack/deploy-edpm.log /home/stack/deploy-edpm.log.$(date +%F-%R)
+sudo ansible-playbook -i /home/stack/tripleo-deploy/edpm-inventory/ \
+  -e @/home/stack/edpm-compute-parameters.yaml \
+  /usr/share/ansible/tripleo-playbooks/deploy-overcloud-compute.yml 2>&1 | tee /home/stack/deploy-edpm.log
